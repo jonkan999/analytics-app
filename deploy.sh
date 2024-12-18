ca492103df9a29779d360534371d42aa923ed489
jonkan <joel.engstrom17@gmail.com>
2024-12-18 08:52:02 +0100

    Deploying latest changes

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,11 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+# Define your variables
+your_project_id="aggregatory-440306"  # Replace with your actual project ID
+your_image_name="analytics-processor-job"  # Use your actual image name
+your_service_name="analytics-processor-job"  # Use your service name
+
 # Step 1: Install Python dependencies
 echo "Installing Python dependencies..."
 pip install -r requirements.txt
@@ -18,16 +23,16 @@ git push
 
 # Step 4: Build the Docker image
 echo "Building the Docker image..."
-docker build -t analytics-processor-job .
+docker build -t $your_image_name .
 
 # Step 5: Push the Docker image to Google Container Registry
 echo "Pushing the Docker image to Google Container Registry..."
-docker tag analytics-processor-job gcr.io/aggregatory-440306/analytics-processor-job
-docker push gcr.io/aggregatory-440306/analytics-processor-job
+docker tag $your_image_name gcr.io/$your_project_id/$your_image_name
+docker push gcr.io/$your_project_id/$your_image_name
 
 # Step 6: Deploy to Cloud Run
 echo "Deploying to Cloud Run..."
-gcloud run deploy your-service-name --image gcr.io/aggregatory-440306/analytics-processor-job --region europe-west3
+gcloud run deploy $your_service_name --image gcr.io/$your_project_id/$your_image_name --region europe-west3
 
 # Step 7: Deploy to Firebase Hosting
 echo "Deploying to Firebase Hosting..."
